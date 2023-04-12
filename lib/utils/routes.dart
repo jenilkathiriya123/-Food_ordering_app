215c4c32ce393d60646ffad652728d44e8826835
jenil kathiriya <6783jenilkathiriya@gmail.com>
2023-04-12 10:58:19 +0530

    Flutter

---
+++ lib/utils/routes.dart
@@ -0,0 +1,13 @@
+
+
+import '../../views/home_screen/page/home_screen.dart';
+import 'package:flutter/cupertino.dart';
+import '../views/intro_screen.dart';
+import '../views/product_page.dart';
+import 'appRoutes.dart';
+
+Map<String, Widget Function(BuildContext)> routes = {
+  AppRoutes().homePage: (context) => const HomePage(),
+  AppRoutes().introScreen: (context) => const IntroScreen(),
+  AppRoutes().productPage: (context) => const ProductPage(),
+};
